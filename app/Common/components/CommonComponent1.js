6794457c2e3f24475eb9d048bf9b68789de80f8c
Noor <noormahammad@users.noreply.github.com>
2020-01-07 20:59:04 -0500

    TouchableHighlight and Button

--- app/Common/components/CommonComponent1.js
+++ app/Common/components/CommonComponent1.js
@@ -1,22 +1,42 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet, TouchableHighlight} from 'react-native';
 
 export default class CommonComponent1 extends Component {
+  onPressed() {
+    console.log("Box pressed!");
+    alert('You pressed me!');
+  }
   render() {
     return (
       <View style={styles.component}>
         <Text style={styles.header}>{this.props.headerText}</Text>
         <View style={styles.container}>
-          <View style={styles.box1}>
-            <Text>First Box</Text>
-          </View>
-          <View style={styles.box2}>
-            <Text>Second Box</Text>
-          </View>
-          <View style={styles.box3}>
-            <Text>Third Box</Text>
-          </View>
+          <TouchableHighlight
+            onPress={this.onPressed}
+            style={styles.box1}
+            underlayColor="white">
+            <View>
+              <Text>First Box</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this.onPressed} style={styles.box2}>
+            <View>
+              <Text style={styles.whiteText}>Second Box</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this.onPressed} style={styles.box3}>
+            <View style={styles.box3}>
+              <Text>Third Box</Text>
+            </View>
+          </TouchableHighlight>
         </View>
+        <Button
+          onPress={() => {
+            alert('You Clicked me!');
+          }}
+          title="I am Button - Click Me"
+          color="#199800"
+        />
       </View>
     );
   }
@@ -44,7 +64,7 @@ const styles = StyleSheet.create({
   },
   box2: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'brown',
     height: 100,
   },
   box3: {
@@ -52,6 +72,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
     height: 100,
   },
+  whiteText: {
+    color: 'white',
+    fontWeight: '500',
+  }
 });
 
 export {CommonComponent1};
