ad670add1e7e03fd00c1859df03877956e4944ca
Noor <noormahammad@users.noreply.github.com>
2020-01-14 12:20:12 -0500

    reamed Component3 to PeopleComponent

--- app/MyNewScreen/index.js
+++ app/MyNewScreen/index.js
@@ -2,6 +2,6 @@
 
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
-import Component3 from './components/Component3';
+import PeopleComponent from './components/PeopleComponent';
 
-export {Component1, Component2, Component3};
+export {Component1, Component2, PeopleComponent};
--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 import {AppRegistry} from 'react-native';
 
 //import App from './App';
-import {Component3} from './app/MyNewScreen';
+import {PeopleComponent} from './app/MyNewScreen';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Component3);
+AppRegistry.registerComponent(appName, () => PeopleComponent);
